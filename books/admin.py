46885ca4572508abc38e5aa5b86c622110d1aed6
Christoph Buelter <info@cbuelter.de>
2021-04-17 21:35:51 +0200

    Add models for Author, Book, OwnedBook

--- books/admin.py
+++ books/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .models import User
-
+from .models import Author, Book, OwnedBook, User
 
 admin.site.register(User, UserAdmin)
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(OwnedBook)
--- books/models.py
+++ books/models.py
@@ -1,7 +1,34 @@
-from django.db import models
-
 from django.contrib.auth.models import AbstractUser
+from django.db import models
 
 
 class User(AbstractUser):
-    pass
+    owned_books = models.ManyToManyField(
+        "books.Book", related_name="owners", through="books.OwnedBook"
+    )
+
+
+class Author(models.Model):
+    full_name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.full_name
+
+
+class Book(models.Model):
+    title = models.CharField(max_length=128)
+    isbn = models.CharField(
+        unique=True, blank=True, null=True, default=None, max_length=32,
+    )
+    authors = models.ManyToManyField("books.Author", related_name="books")
+
+    def __str__(self):
+        return f"{self.title} {self.isbn or ''}"
+
+
+class OwnedBook(models.Model):
+    user = models.ForeignKey("books.User", on_delete=models.CASCADE)
+    book = models.ForeignKey("books.Book", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.user} -> {self.book}"
