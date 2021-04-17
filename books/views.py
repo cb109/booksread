567efbd3a0cfae2a31d7e667ecbce69c1ca1487b
Christoph Buelter <info@cbuelter.de>
2021-04-17 21:54:52 +0200

    Avoid modifying existing Book if authors differ, copy instead

--- books/views.py
+++ books/views.py
@@ -20,6 +20,13 @@ def get_or_create_book(
         author, _ = Author.objects.get_or_create(full_name=author_name)
         authors.append(author)
 
-    book, _ = Book.objects.get_or_create(title=title, isbn=isbn,)
+    book, created = Book.objects.get_or_create(title=title, isbn=isbn)
+    if not created and book.authors != authors:
+        # Instead of modifying existing book, create a copy with this
+        # set of authors instead.
+        book.id = None
+        book.save()
+
     book.authors.set(authors)
+
     return book
