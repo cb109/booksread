0a30aec7231a655faa10dfd7346f5214ee175f1d
Christoph Buelter <info@cbuelter.de>
2021-04-17 22:51:19 +0200

    Add function to search for book on Google Books API

--- books/views.py
+++ books/views.py
@@ -1,9 +1,42 @@
 from typing import List, Optional
 
+import requests
+from django.conf import settings
 from django.db import transaction
 from django.shortcuts import render
 
 from .models import Author, Book, Publisher
+
+
+def search_google_books(
+    isbn: Optional[str] = None,
+    title: Optional[str] = None,
+    author: Optional[str] = None,
+    max_results: Optional[int] = 5,
+    language: Optional[str] = settings.GOOGLE_BOOKS_LANGUAGE_RESTRICT,
+) -> dict:
+    if not any([isbn, title, author]):
+        raise ValueError("Must search by either ISBN or title/author")
+
+    url = settings.GOOGLE_BOOKS_BASE_URL
+    query = "?q="
+    if isbn:
+        query += f"isbn:{isbn}"
+    elif title or author:
+        if title:
+            query += f"intitle:{title}"
+        if title and author:
+            query += ","
+        if author:
+            query += f"inauthor:{author}"
+
+    query += f"&maxResults={max_results}"
+    if language:
+        query += f"&langRestrict={language}"
+    url += query
+
+    response = requests.get(url)
+    return response.json()
 
 
 @transaction.atomic()
