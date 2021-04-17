26d3c72bf364bf097e536b94f76b188065b430fe
Christoph Buelter <info@cbuelter.de>
2021-04-17 21:47:44 +0200

    Rename admin header

--- books/admin.py
+++ books/admin.py
@@ -3,7 +3,10 @@ from django.contrib.auth.admin import UserAdmin
 
 from .models import Author, Book, OwnedBook, User
 
+admin.site.site_header = "BooksRead Admin"
+
 admin.site.register(User, UserAdmin)
+
 admin.site.register(Author)
 admin.site.register(Book)
 admin.site.register(OwnedBook)
