0b8f1095a355ce2b7b1cdc583baa256783dfd660
Christoph Buelter <info@cbuelter.de>
2021-04-17 22:32:56 +0200

    Create publisher implicitly when creating book

--- books/views.py
+++ books/views.py
@@ -3,30 +3,34 @@ from typing import List, Optional
 from django.db import transaction
 from django.shortcuts import render
 
-from .models import Author, Book
+from .models import Author, Book, Publisher
 
 
 @transaction.atomic()
 def get_or_create_book(
-    title: str, author_names: List[str], isbn: Optional[str] = None,
+    title: str,
+    author_names: List[str],
+    isbn: Optional[str] = None,
+    publisher_name: Optional[str] = None,
+    description: Optional[str] = "",
 ):
-    if isbn:
-        book = Book.objects.filter(isbn=isbn).first()
-        if book:
-            return book
+    publisher = None
+    if publisher_name:
+        publisher, _ = Publisher.objects.get_or_create(name=publisher_name)
 
     authors = []
     for author_name in author_names:
         author, _ = Author.objects.get_or_create(full_name=author_name)
-        authors.append(author)
+        authors.append(author.id)
 
-    book, created = Book.objects.get_or_create(title=title, isbn=isbn)
+    book, created = Book.objects.get_or_create(
+        description=description, isbn=isbn, publisher=publisher, title=title,
+    )
     if not created and book.authors != authors:
-        # Instead of modifying existing book, create a copy with this
-        # set of authors instead.
         book.id = None
         book.save()
 
-    book.authors.set(authors)
+    if authors:
+        book.authors.set(authors)
 
     return book
